f2bf4e61ef28d6c6a916b9ac7fa3da78baf2ba33
feelinglight <kolpacc123@mail.ru>
2025-08-28 22:24:58 +0500

    Добавлены аккорды на квадратные скобки

--- keyboards/lily58/keymaps/druotoni/keymap.h
+++ keyboards/lily58/keymaps/druotoni/keymap.h
@@ -3,12 +3,13 @@
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM MO3_combo[] = {KC_BSPC, MO(2), COMBO_END};
-// const uint16_t PROGMEM alt_shift_combo[] = {KC_LSFT, MO(2), COMBO_END};
+const uint16_t PROGMEM left_square_bracket_combo[] = {KC_Y, KC_U, COMBO_END};
+const uint16_t PROGMEM right_square_bracket_combo[] = {KC_H, KC_J, COMBO_END};
 
 combo_t key_combos[] = {
-    COMBO(MO3_combo, MO(4))
-    // ,
-    // COMBO(alt_shift_combo, LALT(KC_LSFT))
+    COMBO(MO3_combo, MO(4)),
+    COMBO(left_square_bracket_combo, KC_LEFT_BRACKET),
+    COMBO(right_square_bracket_combo, KC_RIGHT_BRACKET)
 };
 
 // --------------- key overrides ---------------

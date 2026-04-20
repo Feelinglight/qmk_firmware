e5a65f52dadc34ae2fc844eb2b5580bb7b6808b7
feelinglight <kolpacc123@mail.ru>
2026-04-20 10:44:51 +0500

    Изменение раскладки. Второй слой включается с левой половины клавиатуры

--- keyboards/lily58/keymaps/druotoni/keymap.h
+++ keyboards/lily58/keymaps/druotoni/keymap.h
@@ -2,14 +2,14 @@
 
 #include QMK_KEYBOARD_H
 
-const uint16_t PROGMEM MO3_combo[] = {KC_BSPC, MO(2), COMBO_END};
+const uint16_t PROGMEM MO3_combo[] = {MO(2), KC_LSFT, COMBO_END};
 const uint16_t PROGMEM left_square_bracket_combo[] = {KC_Y, KC_U, COMBO_END};
 const uint16_t PROGMEM right_square_bracket_combo[] = {KC_H, KC_J, COMBO_END};
 
 combo_t key_combos[] = {
     COMBO(MO3_combo, MO(4)),
-    COMBO(left_square_bracket_combo, KC_LEFT_BRACKET),
-    COMBO(right_square_bracket_combo, KC_RIGHT_BRACKET)
+    COMBO(left_square_bracket_combo, LCTL(KC_INS)),
+    COMBO(right_square_bracket_combo, LSFT(KC_INS))
 };
 
 // --------------- key overrides ---------------
